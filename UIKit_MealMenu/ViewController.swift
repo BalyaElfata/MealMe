b2153386784f6391b9019705b005af52988a72a2
Balya Elfata <balyaelfata@yahoo.com>
2024-12-04 20:25:18 +0700

    Initial Commit

---
+++ UIKit_MealMenu/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKit_MealMenu
+//
+//  Created by Balya Elfata on 04/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
