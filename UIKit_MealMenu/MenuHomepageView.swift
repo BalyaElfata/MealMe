1c1d876ba3d354b17e2ea7efcdb6cced912a735c
Balya Elfata <balyaelfata@yahoo.com>
2024-12-04 22:18:00 +0700

    style: changed grid item count to 20

--- UIKit_MealMenu/MenuHomepageView.swift
+++ UIKit_MealMenu/MenuHomepageView.swift
@@ -56,7 +56,7 @@ class MenuHomepageView: UIViewController, UICollectionViewDataSource, UICollecti
     
     // MARK: - UICollectionViewDataSource Methods
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 9
+        return 20
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
