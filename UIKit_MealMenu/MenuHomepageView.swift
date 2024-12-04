f84ed50f792b2a60ca01637bf4b8d67b9079a014
Balya Elfata <balyaelfata@yahoo.com>
2024-12-04 20:48:24 +0700

    feat: add menu button

--- UIKit_MealMenu/MenuHomepageView.swift
+++ UIKit_MealMenu/MenuHomepageView.swift
@@ -1,12 +1,27 @@
 import UIKit
 
 class MenuHomepageView: UIViewController {
+    let menu = UIButton()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupMenu()
     }
-
-
+    
+    func setupMenu() {
+        view.addSubview(menu)
+        
+        menu.configuration = .filled()
+        menu.configuration?.baseBackgroundColor = .systemGray2
+        menu.configuration?.title = "Menu"
+        
+        menu.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            menu.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            menu.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            menu.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor)
+        ])
+    }
 }
 
