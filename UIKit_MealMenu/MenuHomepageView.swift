5ef2b36ab7bbaaabf14445727d840ea26284502e
Balya Elfata <balyaelfata@yahoo.com>
2024-12-04 20:32:45 +0700

    refactor: refactor UIViewController to MenuHomepageView

--- UIKit_MealMenu/MenuHomepageView.swift
+++ UIKit_MealMenu/MenuHomepageView.swift
@@ -1,6 +1,6 @@
 import UIKit
 
-class ViewController: UIViewController {
+class MenuHomepageView: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
--- UIKit_MealMenu/SceneDelegate.swift
+++ UIKit_MealMenu/SceneDelegate.swift
@@ -13,7 +13,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         window = UIWindow(frame: windowScene.coordinateSpace.bounds)
         window?.windowScene = windowScene
-        window?.rootViewController = ViewController()
+        window?.rootViewController = MenuHomepageView()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
