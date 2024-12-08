8f9ced4b72df7522ca5773e58d87e9836d283fa0
Balya Elfata <balyaelfata@yahoo.com>
2024-12-08 18:00:49 +0700

    feat: added ingredients data from the first ingredient

--- UIKit_MealMenu/MenuData.swift
+++ UIKit_MealMenu/MenuData.swift
@@ -10,15 +10,18 @@ struct Menu: Codable {
         case name = "strMeal"
         case label = "strArea"
         case image = "strMealThumb"
+        case ingredients = "strIngredient1"
     }
     
     var name: String
     var label: String
     var image: String
+    var ingredients: String
     
     init(from menuData: MenuData) {
         self.name = menuData.meals.first?.name ?? ""
         self.label = menuData.meals.first?.label ?? ""
         self.image = menuData.meals.first?.image ?? ""
+        self.ingredients = menuData.meals.first?.ingredients ?? ""
     }
 }
