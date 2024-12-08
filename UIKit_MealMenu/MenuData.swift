d25ff3c66d4d1af8bb49d6df6dbdb17bda8ba029
Balya Elfata <balyaelfata@yahoo.com>
2024-12-08 18:01:33 +0700

    feat: added instructions data

--- UIKit_MealMenu/MenuData.swift
+++ UIKit_MealMenu/MenuData.swift
@@ -11,17 +11,20 @@ struct Menu: Codable {
         case label = "strArea"
         case image = "strMealThumb"
         case ingredients = "strIngredient1"
+        case instructions = "strInstructions"
     }
     
     var name: String
     var label: String
     var image: String
     var ingredients: String
+    var instructions: String
     
     init(from menuData: MenuData) {
         self.name = menuData.meals.first?.name ?? ""
         self.label = menuData.meals.first?.label ?? ""
         self.image = menuData.meals.first?.image ?? ""
         self.ingredients = menuData.meals.first?.ingredients ?? ""
+        self.instructions = menuData.meals.first?.instructions ?? ""
     }
 }
