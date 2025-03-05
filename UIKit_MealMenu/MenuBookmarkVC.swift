deb156ced25bed8351435ac58fe2c935921ec464
Balya Elfata <balyaelfata@yahoo.com>
2025-03-05 17:17:03 +0700

    feat: menu bookmark initial setup

--- UIKit_MealMenu/MenuBookmarkVC.swift
+++ UIKit_MealMenu/MenuBookmarkVC.swift
@@ -1,7 +1,30 @@
 import UIKit
+import CoreData
 
-class MenuBookmarkVC: UIViewController {
+class MenuBookmarkVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    private var menu = [SavedMealItem]()
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
+    private let tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return tableView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.addSubview(tableView)
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.frame = view.bounds
     }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        UITableViewCell()
+    }
+    
 }
