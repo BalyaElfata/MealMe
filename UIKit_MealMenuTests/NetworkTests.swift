a894286ed943dfa72aa3f9748aee3b3b3f30804e
Balya Elfata <balyaelfata@yahoo.com>
2025-03-09 10:02:03 +0700

    test: setup initial network test

--- UIKit_MealMenuTests/NetworkTests.swift
+++ UIKit_MealMenuTests/NetworkTests.swift
@@ -9,21 +9,22 @@ import XCTest
 @testable import UIKit_MealMenu
 
 final class NetworkTests: XCTestCase {
+    var urlString: String!
+    var url: URL!
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        try super.setUpWithError()
+        urlString = "https://www.themealdb.com/api/json/v1/1/search.php?s=chicken"
+        url = URL(string: urlString)
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        urlString = nil
+        url = nil
     }
 
     func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+        
     }
 
     func testPerformanceExample() throws {
