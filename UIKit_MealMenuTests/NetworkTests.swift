91301394ec35e05694ef0320cf23943af348ca68
Balya Elfata <balyaelfata@yahoo.com>
2025-03-09 10:01:48 +0700

    refactor: refactor unit test to NetworkTests

--- UIKit_MealMenuTests/NetworkTests.swift
+++ UIKit_MealMenuTests/NetworkTests.swift
@@ -8,7 +8,7 @@
 import XCTest
 @testable import UIKit_MealMenu
 
-final class UIKit_MealMenuTests: XCTestCase {
+final class NetworkTests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
