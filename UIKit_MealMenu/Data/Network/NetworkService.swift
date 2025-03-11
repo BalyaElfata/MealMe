64a52b9813482e1ba52ce4a64ac467a1845fa307
Balya Elfata <balyaelfata@yahoo.com>
2025-03-11 08:34:28 +0700

    refactor: refactor MenuService to NetworkService

--- UIKit_MealMenu/Data/Network/NetworkService.swift
+++ UIKit_MealMenu/Data/Network/NetworkService.swift
@@ -1,43 +1,19 @@
 import Foundation
 
-//class NetworkService {
-//    private let url = "https://www.themealdb.com/api/json/v1/1/search.php?s=chicken"
-//
-//    func getMenus() async throws -> MenuData {
-//        guard let url = URL(string: url) else {
-//            throw NetworkError.invalidURL
-//        }
-//
-//        let (data, response) = try await URLSession.shared.data(from: url)
-//
-//        guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
-//            throw NetworkError.invalidResponse
-//        }
-//
-//        do {
-//            let decoder = JSONDecoder()
-//            return try decoder.decode(MenuData.self, from: data)
-//        } catch {
-//            throw NetworkError.invalidData
-//        }
-//    }
-//}
-
-class MenuService {
+class NetworkService {
     private let url = "https://www.themealdb.com/api/json/v1/1/search.php?s=chicken"
 
     func getMenus() async throws -> MenuData {
-        
         guard let url = URL(string: url) else {
             throw NetworkError.invalidURL
         }
-        
+
         let (data, response) = try await URLSession.shared.data(from: url)
-        
+
         guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
             throw NetworkError.invalidResponse
         }
-        
+
         do {
             let decoder = JSONDecoder()
             return try decoder.decode(MenuData.self, from: data)
