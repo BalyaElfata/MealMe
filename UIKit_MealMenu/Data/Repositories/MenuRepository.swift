d124d3da9f3e3187df4d68da195901c4ab810ef3
Balya Elfata <balyaelfata@yahoo.com>
2025-03-11 09:45:33 +0700

    fix: adjusted MenuRepository to transform data into Menu model and handle data decoding

--- UIKit_MealMenu/Data/Repositories/MenuRepository.swift
+++ UIKit_MealMenu/Data/Repositories/MenuRepository.swift
@@ -1,42 +1,26 @@
 import Foundation
 
-protocol MenuRepository {
-    func fetchMenus(for id: String, completion: @escaping (Menu?) -> Void) async throws
-}
-
-class DefaultMenuRepository: MenuRepository {
+class MenuRepository {
     private let networkService: NetworkService
     
     init(networkService: NetworkService) {
         self.networkService = networkService
     }
     
-    func fetchMenus(for id: String, completion: @escaping (Menu?) -> Void) async throws {
-        let urlString = "https://www.themealdb.com/api/json/v1/1/lookup.php?i=\(id)"
+    func fetchMenu() async throws -> Menu {
+        let urlString = "https://www.themealdb.com/api/json/v1/1/search.php?s=chicken"
+        let data = try await networkService.fetchData(from: urlString)
         
-        guard let url = URL(string: urlString) else {
-            completion(nil)
-            return
+        let response = try JSONDecoder().decode(MenuResponse.self, from: data)
+        
+        guard let menu = response.meals.first else {
+            throw MenuError.notFound
         }
         
-        URLSession.shared.dataTask(with: url) { data, response, error in
-            guard let data = data, error == nil else {
-                print("Network error: \(error?.localizedDescription ?? "Unknown error")")
-                completion(nil)
-                return
-            }
-            
-            do {
-                let response = try JSONDecoder().decode(MenuData.self, from: data)
-                if let firstMeal = response.meals.first {
-                    completion(firstMeal)
-                } else {
-                    completion(nil)
-                }
-            } catch {
-                print("Decoding error: \(error)")
-                completion(nil)
-            }
-        }.resume()
+        return menu
+    }
+    
+    enum MenuError: Error {
+        case notFound
     }
 }
